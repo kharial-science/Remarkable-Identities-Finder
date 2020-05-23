d239f3af47219658d3c4a2e5ffc006f5bfdbdbd9
Kharoh <Gaulu03@gmail.com>
2020-05-23 21:48:55 +0200

    computing a layer of the pascal's triangle

---
+++ main.js
@@ -0,0 +1,34 @@
+console.log(computeLayer(4))
+
+
+/**
+ * Compute a given pascal's triangle layer
+ * @param {number} layerNumber - The power of the layer we want to work out, a + b ^ 0 i.e. 0 is the first layer
+ */
+function computeLayer(layerNumber) {
+  let currentLayerNumber = 0
+  let currentLayer = [1]
+
+  while (currentLayerNumber < layerNumber) {
+    currentLayer = computeNextLayer(currentLayer)
+    currentLayerNumber++
+  }
+
+  return currentLayer
+}
+
+/**
+ * Compute the next layer of the pascal's triangle given the current layer
+ * @param {number} currentLayer - The current layer computed, stored in memory
+ */
+function computeNextLayer(currentLayer) {
+  const nextLayer = [1]
+
+  for (let i = 1; i < currentLayer.length; i++) {
+    nextLayer.push(currentLayer[i] + currentLayer[i - 1])
+  }
+
+  nextLayer.push(1)
+
+  return nextLayer
+}
